22041aa61aeb0c941d3eb9049c2ae725513111fe
Justin Field <fieldju@gmail.com>
2019-04-29 09:52:29 -0700

    Update travis config to deploy docker image on release.

--- docker-deploy.sh
+++ docker-deploy.sh
@@ -1,6 +1,12 @@
 #!/usr/bin/env bash
 
-./gradlew clean sJ
+set +x
+set +v
+set -e
+set -o pipefail
+
+export CERBERUS_DOCKER_USER
+export CERBERUS_DOCKER_PASSWORD
 
 VERSION=$(cat gradle.properties | grep version | cut -d'=' -f2)
 
@@ -8,5 +14,10 @@ echo "Version: ${VERSION}"
 
 IMAGE_NAME=cerberusoss/cerberus-lifecycle-management-cli
 
+echo "${CERBERUS_DOCKER_PASSWORD}" | docker login -u ${CERBERUS_DOCKER_USER} --password-stdin
 docker build -t ${IMAGE_NAME}:latest -t ${IMAGE_NAME}:${VERSION} .
 docker push ${IMAGE_NAME}
+
+docker logout
+unset CERBERUS_DOCKER_PASSWORD
+unset CERBERUS_DOCKER_USER
