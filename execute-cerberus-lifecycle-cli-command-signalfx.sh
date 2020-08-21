12c472766246c6d6a495be41a61d6007c00b2b11
Todd Underwood <todd.underwood@nike.com>
2020-08-21 11:34:47 -0700

    feat: Allows us to specify a region to execute the cli in when using the provided scripts

--- execute-cerberus-lifecycle-cli-command-signalfx.sh
+++ execute-cerberus-lifecycle-cli-command-signalfx.sh
@@ -3,17 +3,23 @@
 set -e
 
 echo "ENVIRONMENT=${ENVIRONMENT}"
+echo "REGION=${REGION}"
+
+if [ -z ${REGION} ]; then
+  REGION="us-west-2"
+fi
 
 IP_ADDRESS=$(curl -s http://checkip.amazonaws.com/)
 echo ""
 echo "-----------------------------------------------------------------------------------------------------------------"
 echo ""
-echo "Executing command: '${COMMAND} ${ADDITIONAL_COMMAND_OPTIONS}' in environment: '${ENVIRONMENT}' with the following external IP address: ${IP_ADDRESS}"
+echo "Executing command: '${COMMAND} ${ADDITIONAL_COMMAND_OPTIONS}' in environment: '${ENVIRONMENT}' and region '${REGION}
+ with the following external IP address: ${IP_ADDRESS}"
 echo ""
 echo "-----------------------------------------------------------------------------------------------------------------"
 echo ""
 
-cerberus -e ${ENVIRONMENT} --no-tty ${COMMAND} ${ADDITIONAL_COMMAND_OPTIONS}
+cerberus -e ${ENVIRONMENT} -r ${REGION} --no-tty ${COMMAND} ${ADDITIONAL_COMMAND_OPTIONS}
 
 curl -s -X POST \
   https://ingest.signalfx.com/v2/datapoint \
